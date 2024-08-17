7a9d157f5f47ec013b7c5700e3e3f7e7c9305beb
Pasha Malik <pashamalik9371@gmail.com>
2024-08-17 12:05:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const path = require("path")
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, '/public/'))); // Menyajikan file statis dari folder public
+app.use(express.static(path.join(__dirname, 'public/'))); // Menyajikan file statis dari folder public
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'view/index'));
+    res.sendFile(path.join(__dirname, 'public', 'view/index'));
 });
 app.get('/team', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'view/team'));
+    res.sendFile(path.join(__dirname, 'public', 'view/team'));
 });
 app.get('/programs', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'view/programs'));
+    res.sendFile(path.join(__dirname, 'public', 'view/programs'));
 });
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'view/contact'));
+    res.sendFile(path.join(__dirname, 'public', 'view/contact'));
 });
 const port = process.env.PORT || 3000;
 
