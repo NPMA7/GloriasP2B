5871cc062accd7a9b5e630aec7420d79976b143d
Pasha Malik <pashamalik9371@gmail.com>
2024-08-17 12:20:40 +0700

    fix view

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
 const app = express();
-
 
 app.use(express.static(path.join(__dirname, 'public/'))); // Menyajikan file statis dari folder public
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'view/index'));
+    res.sendFile(path.join(__dirname, 'public', 'view/index.html'));
 });
 app.get('/team', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'view/team'));
+    res.sendFile(path.join(__dirname, 'public', 'view/team.html'));
 });
 app.get('/programs', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'view/programs'));
+    res.sendFile(path.join(__dirname, 'public', 'view/programs.html'));
 });
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'view/contact'));
+    res.sendFile(path.join(__dirname, 'public', 'view/contact.html'));
 });
 const port = process.env.PORT || 3000;
 
