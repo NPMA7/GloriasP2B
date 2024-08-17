d613f045f6add10e127d1342ea253f49e7cc3fc7
Pasha Malik <pashamalik9371@gmail.com>
2024-08-17 11:56:23 +0700

    not include Image

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const path = require("path")
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '/public/'))); // Menyajikan file statis dari folder public
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'view/index'));
+});
+app.get('/team', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'view/team'));
+});
+app.get('/programs', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'view/programs'));
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'view/contact'));
+});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
